e52873a85bad349e6dbb81e28e660c84edf6e20c
Adrian Szymański <aszymanski@infusion.com>
2015-05-05 15:49:45 +0200

    Updated JUnit_example - Test Suite feature

---
+++ Appium/JUnit_example/src/main/java/Example_report/JUnit_example/JunitTestSuite.java
@@ -0,0 +1,20 @@
+package Example_report.JUnit_example;
+
+import org.junit.runner.RunWith;
+
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+
+	Calculator.class,
+	Calculator2.class
+
+})
+
+
+
+public class JunitTestSuite {
+
+}
